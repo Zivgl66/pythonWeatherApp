dbf5a0fa85855fc1efcb2d5f63d64c522342934f
ziv.gliser <zivgl66@gmail.com>
2024-02-07 08:36:28 +0200

    added ws2

---
+++ c/ws2/ex3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+static int s_i = 7;
+int i = 7;
+int *ptr = &i;
+int *ptr2 = (int *)malloc(sizeof(int));
+int **ptr3;
+
+if(ptr)
+{
+	ptr3 = &ptr;
+}
+
+printf("%p\n" , (void *)&s_i);
+printf("%p\n" , (void *)&i);
+printf("ptr: %p\n" , (void *)ptr);
+printf("ad ptr: %p\n", (void *)&ptr);
+printf("ptr2: %p\n" , (void *)ptr2);
+printf("ptr3: %p\n" , (void *)ptr3);
+
+
+free(ptr2);
+
+
+return(0);
+}
