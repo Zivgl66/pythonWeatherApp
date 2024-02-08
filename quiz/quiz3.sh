0f2ba1e1c2344cd3da48a4089659ab9beb486c2d
ziv.gliser <zivgl66@gmail.com>
2024-02-08 11:40:00 +0200

    quiz3 added

---
+++ quiz/quiz3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+student_array=("Ziv" "Tal" "Yossi" "Yochai" "Liav" "Oren" "Sagi" "Sergei" "Maxim" "Dennis" "Romi" "Bar" )
+student_array=( $(shuf -e "${student_array[@]}") )
+arr_size=${#student_array[@]}
+for(( i=0; i < $arr_size; i+=2))
+do
+	if (($arr_size - $i == 3))
+	then
+		echo "Partners: ${student_array[$i]} - ${student_array[$i + 1]} - ${student_array[$i + 2]}"
+		break
+	fi
+	echo "Partners: ${student_array[$i]} - ${student_array[$i + 1]}"
+done
+
