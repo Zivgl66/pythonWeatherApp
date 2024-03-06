a7d367651232564c512ad3ed4ce9517a05423030
ziv.gliser <zivgl66@gmail.com>
2024-03-06 17:21:40 +0200

    python - files and context managers exercises added

--- python/Python_ScopesAndModules/getfileper.py
+++ python/Python_ScopesAndModules/getfileper.py
@@ -1,21 +1,24 @@
-import os
-import stat
-import sys
+import os # import for operating system file search and stat module
+import sys # import for the arguments in command line
 # exercise5:
 
-
+'''
+Find the file path of a given file name
+'''
 def find_files(filename):
     for root, dirs, files in os.walk("/"):
         if filename in files:
             return os.path.join(root, filename)
     return None
 
-
+'''
+if File was found in the system, check it's x permissions, if none, add x to owner and group permissions
+'''
 def find_and_perm(file):
     file_path = find_files(file)
     if file_path:
         if not os.access(file, os.X_OK):
-            os.chmod(file_path, os.stat(file_path).st_mode | stat.S_IXGRP | stat.S_IXUSR)
+            os.chmod(file_path, os.stat(file_path).st_mode | os.stat.S_IXGRP | os.stat.S_IXUSR)
             print("File Permissions changed!")
         else:
             print("File has permissions!")
