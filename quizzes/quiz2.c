28001a36f5cbc08bf0af4a1251cb436ba0e7d8eb
ziv.gliser <zivgl66@gmail.com>
2024-03-07 08:51:47 +0200

    comments added

---
+++ quizzes/quiz2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc , char *argv[])
+{
+if( -1 < (unsigned char)1)
+{
+printf("A");
+}
+else 
+{
+printf("B");
+}
+
+if(-1 < (unsigned int)1)
+printf("A");
+else
+printf("B");
+
+
+return(0);
+}
