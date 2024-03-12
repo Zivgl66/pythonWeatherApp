22ed2b6be8162884be97d30096c6a7a7f7e1e08f
ziv.gliser <zivgl66@gmail.com>
2024-03-12 09:55:50 +0200

    added today's quiz

---
+++ quizzes/is Palindrom.py
@@ -0,0 +1,18 @@
+# def is_palindrome(num):
+#     num_len = len(str(num))
+#     while num_len > 2:
+#         if int(str(num)[:1]) != num % 10:
+#             return False
+#         num = int(str(num)[1:-1])
+#         num_len -= 2
+#     if num_len == 2 and int(str(num)[:1]) == num % 10 or num_len == 1:
+#         return True
+#     else:
+#         return False
+
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+
+print(is_palindrome(2112))
